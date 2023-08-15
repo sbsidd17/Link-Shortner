4d01f102dae177c22d8f9d891cb7ab2524cf471f
Shuaib Siddiqui <sbsidd17@gmail.com>
2023-08-15 16:25:01 +0530

    m

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,12 @@ import userRoutes from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  }));
+  
 app.use(express.json())
 app.use(cookieParser())
 
