ffef7166c572ed5530d64365d8eb1bf6d1317d48
Shuaib Siddiqui <sbsidd17@gmail.com>
2023-08-15 16:09:53 +0530

    add cors origin

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,10 @@ import userRoutes from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
-app.use(cors({}))
+app.use(cors({
+    origin:process.env.CLIENT_URL, 
+    credentials:true
+}))
 app.use(express.json())
 app.use(cookieParser())
 
